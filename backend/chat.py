c996448c2d34c30dd778cc832480bc112320685d
schen5632 <93620334+schen5632@users.noreply.github.com>
2024-09-27 00:46:23 -0400

    Update chat.py

--- backend/chat.py
+++ backend/chat.py
@@ -20,11 +20,11 @@ all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
 
-    
+# load trained model from file that was trained using train.py
 FILE = "data.pth"
 data = torch.load(FILE)
 
-
+# create new neural network with parameters of the trained model 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
@@ -37,16 +37,19 @@ def get_response(msg):
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
+    # predict tags from input
     output = model(X)
     _, predicted = torch.max(output, dim=1)
 
     tag = tags[predicted.item()]
 
+    # softmax function to convert raw scores to probabilities
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.8:
         for intent in intents['intents']:
             if tag == intent["tag"]:
+                # return random response based on predicted tag
                 return random.choice(intent['responses'])
     return "Sorry, I don't quite understand what you mean"
 
